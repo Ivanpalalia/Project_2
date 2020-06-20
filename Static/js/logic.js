ff2dea6fefc44bd8a37386bbda0c78cf19104c84
Ronald <ho@smcl.org>
2020-06-20 13:05:59 -0700

    added location

--- Static/js/logic.js
+++ Static/js/logic.js
@@ -10,3 +10,19 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     id: "mapbox/streets-v11", 
     accessToken: API_KEY
 }).addTo(map);
+
+var url = "https://datahub.smcgov.org/resource/pjzf-pe8z.json"
+
+d3.json(url, function(response) {
+
+  console.log(response);
+
+  for (var i = 0; i < response.length; i++) {
+    var location = response[i].location_2;
+
+    if (location) {
+      L.marker([location.coordinates[1], location.coordinates[0]]).addTo(map);
+    }
+  }
+
+});
