57765eb1f7e802e6ba1af84baea5de9b6c0cf437
m1ckeyt <michael.trias@gmail.com>
2020-06-29 18:34:23 -0700

    updated plots.js

--- Brainstorm/plots.js
+++ Brainstorm/plots.js
@@ -1,30 +1,18 @@
 
-// const url = "https://datahub.smcgov.org/resource/pjzf-pe8z.json"
-// const url = "RestaurantScores.geojson"
-
-//used a cleaned up dataset to test, will connect to database once functionality works
+//define data set
 const url = "sf_cleaned_data3.json"
 
 
-// console.log(restPerZip)
-
-// function init(){
-    //grabs reference to the dropdown element
+//grabs reference to the dropdown element and updates chart and table based on change
 var zipCode = d3.select("#selDataset").on("change", updatePlotly);
 
-function updatePlotly() {
-  
+function updatePlotly() {  
     d3.json(url).then(buildCharts)
 }
 
-
-// d3.select("#selDataset").property("value",123)
-
-function optionChanged(zipCode) {
-   
-    console.log(zipCode);
-    
-  }
+function optionChanged(zipCode) {   
+    console.log(zipCode);  
+}
 
 
 function buildCharts(data){
@@ -44,32 +32,14 @@ var restNumb =[]
     function findZip(zip){
         return zip.business_postal_code === zipValue;
     }
-    // function findTop(top){
-    //     return top.inspection_score > 95;
-    // }
-    // function findBottom(bottom){
-    //     return bottom.inspection_score <70;
-    // }
-
+    
     var filteredZip = data.filter(findZip);
-    // var filteredTop = data.filter(findTop);
-    // var filteredBottom = data.filter(findBottom);
-    // var filteredBottom2 = filteredBottom.slice(0,19)
-
+    
     console.log(filteredZip.length);
 
     
     
-    //sort top 20 of filteredZip
-
-    // var Top_filter = filteredZip.sort(function(a, b){return a-b});
-    // Top_filter = Top_filter.slice(0,20);
-
-    // for (i=0; i<filteredBottom2.length;i++){
-    //     // console.log(filteredBottom2[i].inspection_score)
-    // }
-    
-    //
+    // pushes name,score and postalCode to new arrays
     for (i=0; i< (Object.keys(filteredZip).length);i++) {
         var name = filteredZip[i].business_name;
         var score = filteredZip[i].inspection_score;
@@ -77,13 +47,10 @@ var restNumb =[]
     
         name_array.push(name)
         score_array.push(score)
-        restPerZip.push(postalCode)
-
-       
+        restPerZip.push(postalCode)  
     }
-
       
-    // determine # of zipcodes create object
+    // creates an object for the # of unique zipcodes 
 
     const counts = Object.create(null);
 
@@ -106,7 +73,8 @@ var restNumb =[]
 
 
 
-    // make bar chart
+    // makes the bar chart
+    //limits to only 100 data points
 
     var score2 = score_array.slice(0,100)
     var name2 = name_array.slice(0,100)
@@ -115,20 +83,19 @@ var restNumb =[]
     
     var graph = [{
         
-        // y:filteredBottom2.inspection_score,
-        // x:filteredBottom2.business_name,
         y: score2,
         x: name2,
         type: 'bar',
         width: 0.5
-
-        
-       
+  
     }];
 
     var layout = {
         
         title: 'SF Restaurant Scores',
+        yaxis:{
+          automargin: true
+        },
         xaxis: {
           tickangle: -45
         },
@@ -140,7 +107,38 @@ var restNumb =[]
       
     Plotly.newPlot('bar', graph, layout);
 
-    //create bubble chart 
+    
+  //Creates a table with description of violation,name, health score, etc.. based on zipcode
+
+   //clears table data 
+  tbody.html("");
+
+  // Next, loop through each object in the data
+  // and append a row and cells for each value in the row
+  filteredZip.forEach((dataRow) => {
+  // Append a row to the table body
+    const row = tbody.append("tr");
+
+  // Loop through each field in the dataRow and add
+  // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+      let cell = row.append("td");
+        cell.text(val);
+      }
+    );
+  });
+
+
+    
+}
+
+const tbody = d3.select("tbody");
+
+d3.json(url).then(buildCharts)
+
+
+//----extra
+//create bubble chart 
 
     //     var bubbleLayout = {
     //         title: "SF Restaurant Scores",
@@ -182,32 +180,4 @@ var restNumb =[]
       
     // Plotly.newPlot('bubble', data, layout);
 
-    //Details table
-
-   //clears table data 
-
-    tbody.html("");
-
-    // Next, loop through each object in the data
-  // and append a row and cells for each value in the row
-  filteredZip.forEach((dataRow) => {
-    // Append a row to the table body
-    const row = tbody.append("tr");
-
-    // Loop through each field in the dataRow and add
-    // each value as a table cell (td)
-    Object.values(dataRow).forEach((val) => {
-      let cell = row.append("td");
-        cell.text(val);
-      }
-    );
-  });
-
-
-    
-}
-
-const tbody = d3.select("tbody");
-
-d3.json(url).then(buildCharts)
 
